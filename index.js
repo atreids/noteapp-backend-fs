2bb78bf0288e23a6b025f575c267b440dc8b0f89
Aaron Donaldson <atreids@outlook.com>
2022-06-10 14:20:38 +0100

    part3 deploy stage

---
+++ index.js
@@ -0,0 +1,96 @@
+const { response } = require("express");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.send(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.sendStatus(400).json({
+      error: "missing content",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
