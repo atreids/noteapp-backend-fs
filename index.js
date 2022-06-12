af999db232f97b3df1d6db4f0f6b1888eb917123
Aaron Donaldson <aaron-donaldson@github.com>
2022-06-12 15:40:29 +0100

    uibuild

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const Note = require("./models/notes");
+const Note = require("/models/notes");
 
 app.use(express.json());
 app.use(cors());
